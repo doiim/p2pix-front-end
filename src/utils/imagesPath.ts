976c48ac4b2c89b054f7fefc3ece8d5287f5049c
hueso <hueso@torguen.com.ar>
2025-10-12 18:57:25 -0300

    optimized icon importing

--- src/utils/imagesPath.ts
+++ src/utils/imagesPath.ts
@@ -1,29 +1,11 @@
 import type { TokenEnum } from "@/model/NetworkEnum";
-
-export const imagesPath = import.meta.glob<string>("@/assets/*.{png,svg}", {
-  eager: true,
-  query: "?url",
-  import: "default",
-});
+import { Networks } from "@/config/networks";
 
 export const getNetworkImage = (networkName: string): string => {
-  const imageName = networkName
-      .toLowerCase()
-      .replace(/[^a-z0-9]/g, '-');
-  try {
-    const path = Object.keys(imagesPath).find((key) =>
-      key.endsWith(`${imageName}.svg`)
-    );
-    return path ? imagesPath[path] : "";
-  } catch (error) {
-    console.error("Error fetching network image");
-    return "";
-  }
+  const normalizedName = networkName.toLowerCase().replace(/[^a-z0-9]/g, '-');
+  return new URL(`../assets/networks/${normalizedName}.svg`, import.meta.url).href;
 };
 
 export const getTokenImage = (tokenName: TokenEnum): string => {
-  const path = Object.keys(imagesPath).find((key) =>
-    key.endsWith(`${tokenName.toLowerCase()}.svg`)
-  );
-  return path ? imagesPath[path] : "";
+  return new URL(`../assets/tokens/${tokenName.toLowerCase()}.svg`, import.meta.url).href;
 };
